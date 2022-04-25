bb810fd1dcf76364644d0f73f3b44cfde9471343
Desislava Todorova <desi_14_94@abv.bg>
2022-04-25 16:45:29 +0300

    format

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -5,32 +5,37 @@ import { IBook } from '../interfaces/book.interface';
 import { CreatedBook } from '../interfaces/createdBook.interface.';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+export class BookService {
+  constructor(private httpClient: HttpClient) {}
 
-export class BookService{
+  getBooks(): Observable<any> {
+    return this.httpClient.get<IBook>(
+      'https://murmuring-cliffs-61613.herokuapp.com/books'
+    );
+  }
 
-    constructor(private httpClient: HttpClient) { }
+  getBook(id: string): Observable<any> {
+    return this.httpClient.get<IBook>(
+      `https://murmuring-cliffs-61613.herokuapp.com/books/${id}`
+    );
+  }
 
-    getBooks(): Observable<any> {
-      return this.httpClient.get<IBook>('https://murmuring-cliffs-61613.herokuapp.com/books');
-    }
-
-    getBook(id: string) : Observable<any>{
-      return this.httpClient.get<IBook>(`https://murmuring-cliffs-61613.herokuapp.com/books/${id}`);
-    }
-
-    addBook(body: CreatedBook, token: string){
-      return this.httpClient.post('https://murmuring-cliffs-61613.herokuapp.com/books/create', {
-        headers: { "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
+  addBook(body: CreatedBook, token: string) {
+    return this.httpClient.post(
+      'https://murmuring-cliffs-61613.herokuapp.com/books/create',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
         },
-        body: body
-      } );
-    }
+        body: body,
+      }
+    );
+  }
 
-    deleteBook(id: string) {
-      return this.httpClient.delete(`http://localhost:3000/books/${id}`);
-    }
-
+  deleteBook(id: string) {
+    return this.httpClient.delete(`http://localhost:3000/books/${id}`);
+  }
 }
